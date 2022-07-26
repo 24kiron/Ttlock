8bdf9160b359645525576d4ce7f399837d65e56d
hmx089674 <2151320248@qq.com>
2022-07-26 15:46:49 +0800

    新增锁相关接口

--- README.md
+++ README.md
@@ -8,6 +8,56 @@
 ```
 composer require hemingxing/ttlock
 ```
-### Ttlock api
+## Ttlock/api （均为post请求）
+```
+-- config 服务参数配置
+```
+```
+-- getAccessToken 获取访问令牌
+返回参数为json字符串，结果如下：
+    {
+         "access_token": "39caac89b0b51c980aa61ad4264b693b", //访问令牌
+         "uid": 2340,//用户主键id
+         "refresh_token": "1bd2a21a7df889630f444364813738d7",//刷新令牌
+         "expires_in": 7776000,//访问令牌过期时间，单位秒
+     }
+```
+```
+-- refreshAccessToken 刷新访问令牌
+```
+###密码相关接口：
+```
+-- getRandPwdUnlock 获取开锁随机密码（从云端获取一个6-9位数字的随机密码用于开锁，密码由云端的算法规则生成，不需要锁连接网关就可以获取，位数主要由密码类型和有效期长短决定，密码随机生成，不可定制)
+```
+```
+-- getRandPwdUnlock 添加自定义密码（向锁里写入一个4-9位的自定义限期密码)
+```
+```
+-- getListKeyboardPwd 获取锁的密码列表（管理员或授权管理员查看这把锁上生成的所有密码，包含随机密码和自定义密码。)
+返回参数：
+{
+    "list": [
+        {
+            "keyboardPwdId": 3234293, //键盘密码ID
+            "lockId": 532323,//锁ID
+            "keyboardPwd": "034234",//键盘密码
+            "keyboardPwdName":"陈文亮",//键盘密码名称
+            "keyboardPwdType": "3",//键盘密码类型（请自行前往开放平台查看对应类型内容）
+            "startDate": 1528878944000,//有效期开始时间（时间戳，单位毫秒）
+            "endDate": 1628878944000,//有效期结束时间（时间戳，单位毫秒）
+            "sendDate": 1528878944000,//发送时间（时间戳，单位毫秒）
+            "senderUsername": "alexa@google.com"//发送者用户名
+        }
+    ],//记录列表
+    "pageNo":1,//页码，从1开始
+    "pageSize":20,//每页数量，最大100
+    "pages":1,//总页数
+    "total":1//总条数
+}
+```
+```
+-- updateKeyboardPwd 修改密码（修改之前生成的随机密码或自定义密码）
+```
+## 写在最后
 
-- getRandPwdUnlock 获取开锁随机密码（参数 lockId)
+感谢您的使用，正在持续开发更多功能中...
--- src/Ttlock.php
+++ src/Ttlock.php
@@ -16,13 +16,34 @@ class Ttlock
     protected $client = null;
     protected $client_id = 'accfeeacda694160890c0170f6a1310f';
     protected $client_secret = 'ca257d5ec7278bee40cc2bd1513103f3';
+    protected $username = '';//开放平台账号（手机号）
+    protected $password = '';//开放平台密码
 
     public function __construct()
     {
         $this->client = new \GuzzleHttp\Client();
     }
 
-    //获取access_token
+    /**
+     * 注册服务参数
+     * @param null $client_id 创建应用分配的client_id
+     * @param null $client_secret 创建应用分配的client_secret
+     * @param null $username 通通锁或通通锁APP的登录账号，也可以是注册用户接口返回的账号。注意：请别使用开发者账号。
+     * @param null $password 账号密码
+     */
+    public function config($client_id = null, $client_secret = null, $username = null, $password = null)
+    {
+        $this->client_id = !empty($client_id) ? $client_id : '';
+        $this->client_secret = !empty($client_secret) ? $client_secret : '';
+        $this->username = !empty($username) ? $username : '';
+        $this->password = !empty($password) ? $password : '';
+    }
+
+
+    /**
+     * 获取access_token
+     * @return string 返回json字符串
+     */
     public function getAccessToken()
     {
         //你还可以提供其他可选的构造函数参数
@@ -33,62 +54,79 @@ class Ttlock
             'form_params' => [
                 'client_id' => $this->client_id,//参数
                 'client_secret' => $this->client_secret,//参数
-                'username' => '+8613885168270',//参数
-                'password' => md5('yfb2022tts'),//参数
+//                'username' => '+8613885168270',//参数
+                'username' => '+86' . $this->username,//参数
+                'password' => md5($this->password),//参数
             ]
         ]);
         //返回值
-        $result = json_decode($response->getBody()->getContents(), true);
-
-        $myfile = fopen("access_token", "w") or die("Unable to open file!");
-        $txt = json_encode([
-            'access_token' => $result['access_token'],
-            'expires_in' => $result['expires_in'] + time()
-        ]);
-        fwrite($myfile, $txt);
-        fclose($myfile);
-        return $result['access_token'];
+        $result = $response->getBody()->getContents();
+        return $result;
     }
 
-    //获取门下锁列表
-    public function getLockerList()
+    /**
+     * @param $refresh_token 刷新令牌
+     * @return string 返回结果
+     */
+    public function refreshAccessToken($refresh_token)
     {
-        $url = 'https://api.ttlock.com/v3/lock/list';
-        try {
-            $access_token_file = json_decode(file_get_contents('./access_token'), true);
-            if ($access_token_file['expires_in'] <= time()) {
-                $access_token = $this->getAccessToken();//刷新token
-            }else{
-                $access_token = $access_token_file['access_token'];
-            }
-            $response = $this->client->post($url, [
-                'headers' => ['Content-type' => 'application/x-www-form-urlencoded'],
-                'form_params' => [
-                    'clientId' => $this->client_id,//参数
-                    'accessToken' => $access_token,//参数
-                    'pageNo' => 1,//参数
-                    'pageSize' => 10,//参数
-                    'date' => time() * 1000,//参数
-                ]
-            ]);
-            //返回值
-            $result = $response->getBody()->getContents();
-            return $result;
-        } catch (ClientException $exception) {
-            return 'error';
-        }
+        //你还可以提供其他可选的构造函数参数
+        $url = 'https://api.ttlock.com/oauth2/token';
+
+        $response = $this->client->post($url, [
+            'headers' => ['Content-type' => 'application/x-www-form-urlencoded'],
+            'form_params' => [
+                'client_id' => $this->client_id,//参数
+                'client_secret' => $this->client_secret,//参数
+                'grant_type' => 'refresh_token',//参数
+                'refresh_token' => $refresh_token,//参数
+            ]
+        ]);
+        //返回值
+        $result = $response->getBody()->getContents();
+        return $result;
     }
 
-    //随机密码：解锁
-    public function getRandPwdUnlock($lockId)
+    /**
+     * 添加自定义密码-仅对已通过App初始化过的门锁生效
+     * @param $lockId 锁ID，锁初始化获取
+     * @param $access_token 访问令牌
+     * @param $keyboardPwd 密码
+     * @param $startDate 有效期开始时间（时间戳）
+     * @param $endDate 有效期开始时间（时间戳)
+     * @return string json字符串
+     */
+    public function createSelfMakeLockPwd($lockId, $access_token, $keyboardPwd, $startDate, $endDate)
+    {
+        $url = 'https://api.ttlock.com/v3/keyboardPwd/add';
+        $response = $this->client->post($url, [
+            'headers' => ['Content-type' => 'application/x-www-form-urlencoded'],
+            'form_params' => [
+                'clientId' => $this->client_id,//参数
+                'accessToken' => $access_token,//参数
+                'lockId' => $lockId,//参数
+                'keyboardPwd' => $keyboardPwd,//参数
+                'startDate' => $startDate * 1000,//参数
+                'endDate' => $endDate * 1000,//参数
+                'date' => time() * 1000,//参数
+            ]
+        ]);
+        $result = $response->getBody()->getContents();
+        return $result;
+
+    }
+
+    /**
+     * 获取锁-临时密码-仅对已通过App初始化过的门锁生效
+     * @param $lockId 锁ID，锁初始化获取
+     * @param $access_token 访问令牌
+     * @param $startDate 有效期开始时间（时间戳）
+     * @param $endDate 有效期结束时间（时间戳）
+     * @return string
+     */
+    public function getRandPwdUnlock($lockId, $access_token, $startDate, $endDate)
     {
         $url = 'https://api.ttlock.com/v3/keyboardPwd/get';
-        $access_token_file = json_decode(file_get_contents('./access_token'), true);
-        if ($access_token_file['expires_in'] <= time()) {
-            $access_token = $this->getAccessToken();//刷新token
-        }else{
-            $access_token = $access_token_file['access_token'];
-        }
         $response = $this->client->post($url, [
             'headers' => ['Content-type' => 'application/x-www-form-urlencoded'],
             'form_params' => [
@@ -96,8 +134,8 @@ class Ttlock
                 'accessToken' => $access_token,//参数
                 'lockId' => $lockId,//参数
                 'keyboardPwdType' => 3,//参数
-                'startDate' => time() * 1000,//参数
-                'endDate' => (time() + 24 * 60 * 60) * 1000,
+                'startDate' => $startDate * 1000,//参数
+                'endDate' => $endDate * 1000,
                 'date' => time() * 1000,//参数
             ]
         ]);
@@ -106,31 +144,56 @@ class Ttlock
         return $result;
     }
 
-    //添加自定义密码，开锁
-    public function selfMakePwd()
+    /**
+     * 获取锁的密码列表-仅对已通过App初始化过的门锁生效
+     * @param $lockId 锁ID，锁初始化获取
+     * @param $access_token 访问令牌
+     * @param $pageNo 页码，从1开始
+     * @param $pageSize 每页数量，最大100
+     * @return string
+     */
+    public function getListKeyboardPwd($lockId, $access_token, $pageNo, $pageSize)
     {
-        $url = "https://api.ttlock.com/v3/keyboardPwd/add";
-        $access_token_file = json_decode(file_get_contents('./access_token'), true);
-        if ($access_token_file['expires_in'] <= time()) {
-            $access_token = $this->getAccessToken();//刷新token
-        }else{
-            $access_token = $access_token_file['access_token'];
-        }
+        $url = "https://api.ttlock.com/v3/lock/listKeyboardPwd";
         $response = $this->client->post($url, [
             'headers' => ['Content-type' => 'application/x-www-form-urlencoded'],
             'form_params' => [
                 'clientId' => $this->client_id,//参数
                 'accessToken' => $access_token,//参数
-                'lockId' => 6070727,//参数
-                'keyboardPwd' => 3,//参数
-                'startDate' => time() * 1000,//参数
-                'endDate' => (time() + 24 * 60 * 60) * 1000,
+                'lockId' => $lockId,//参数
+                'pageNo' => $pageNo,//参数
+                'pageSize' => $pageSize,//参数
                 'date' => time() * 1000,//参数
             ]
         ]);
         //返回值
         $result = $response->getBody()->getContents();
-       return $result;
+        return $result;
+    }
+
+    /**
+     * 修改密码
+     * @param $lockId 锁ID
+     * @param $access_token 访问令牌
+     * @param $keyboardPwdId 键盘密码ID
+     * @return string
+     */
+    public function updateKeyboardPwd($lockId, $access_token, $keyboardPwdId)
+    {
+        $url = "https://api.ttlock.com/v3/keyboardPwd/change";
+        $response = $this->client->post($url, [
+            'headers' => ['Content-type' => 'application/x-www-form-urlencoded'],
+            'form_params' => [
+                'clientId' => $this->client_id,//参数
+                'accessToken' => $access_token,//参数
+                'lockId' => $lockId,//参数
+                'keyboardPwdId' => $keyboardPwdId,//参数
+                'date' => time() * 1000,//参数
+            ]
+        ]);
+        //返回值
+        $result = $response->getBody()->getContents();
+        return $result;
     }
 
 }
